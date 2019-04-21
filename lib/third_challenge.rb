3b29b3cda75dff7373f7d03a236679522a1e3cbd
Tejas Cousik <tcousik0@gmail.com>
2019-04-21 23:11:04 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,7 +12,7 @@ def third_challenge
       :hero => {
         name: "Romeo",
         age: "15",
-        status: "Alive"
+        status: "alive"
         },
       :hero_friends => []
    }, 
@@ -24,7 +24,9 @@ def third_challenge
         #your key/value pairs here
         },
       :heroine => {
-        #your key/value pairs here
+        name: "Juliet",
+        age: "15",
+        status: "alive"
         },
       :heroine_friends => []
    }
